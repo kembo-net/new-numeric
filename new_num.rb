fd10466391e140647d39c46e43a6329d8fdd9919
Kembo <lupin@kembo.org>
2016-05-22 14:28:02 +0900

    ソースコードにコメント挿入（ついでに微修正）

--- new_num.rb
+++ new_num.rb
@@ -1,9 +1,13 @@
 module NewNum
-  class UndefinedOperationError < StandardError
-  end
+  class UndefinedOperationError < StandardError; end
+  #自然数
   class NaturalNum < Numeric
+    #newメソッドは封印
     private_class_method :new
+    #前者と便宜用の数値表示　後者は後で
     attr_reader :val, :prev
+    #引数を前者として新しい数を作る　0の時前者はnil
+    #後者は参照されてから作るので今はnil
     def initialize (prev = nil)
       @next = nil
       @prev = prev
@@ -11,32 +15,30 @@ module NewNum
         @val = 0
       else
         @val = @prev.val + 1
-        @prev.set_next(self)
       end
     end
     @@Zero = nil
+    #ゼロを参照　無かったら作る
     def self.Zero
       @@Zero or @@Zero = new()
     end
     def zero?; @prev.nil?; end
     def to_s; "N[#{@val}]"; end
     def to_i; @val; end
+    #後者を作るメソッド
     def self.create_next(n)
       raise TypeError unless n.kind_of?(self)
-      return n.next if n.next?
       new(n)
     end
 
     def next?; not @next.nil?; end
+    #後者を参照　無ければ作る
     def next
       return @next if next?
-      self.class.create_next(self)
-    end
-    def set_next(n)
-      @next = n
+      @next = self.class.create_next(self)
       freeze
+      @next
     end
-    protected :set_next
 
     def self.[](n)
       raise TypeError unless n.integer? and n >= 0
@@ -44,11 +46,15 @@ module NewNum
       n.times {result = result.next}
       return result
     end
+    #1. a + 0 = a
+    #2. a + suc(b) = suc(a + b)
     def +(other)
       raise TypeError unless other.kind_of?(self.class)
       return self if other.zero?
       return (self + other.prev).next
     end
+    #1. a * 0 = 0
+    #2. a * suc(b) = a * b + a
     def *(other)
       raise TypeError unless other.kind_of?(self.class)
       return @@Zero if other == @@Zero
@@ -59,14 +65,16 @@ module NewNum
       x = @@Zero
       while true
         case [x.equal?(self), x.equal?(other)]
-        when [true, true]  then break  0
-        when [true, false] then break -1
-        when [false, true] then break  1
+        when [true , false] then break -1
+        when [true , true ] then break  0
+        when [false, true ] then break  1
         end
         x = x.next
       end
     end
 
+    #1'. a + 0 = a  =>  a - 0 = a
+    #2'. suc(a) - suc(b) = a - b
     def -(other)
       raise TypeError unless other.kind_of?(self.class)
       if self < other
@@ -74,6 +82,7 @@ module NewNum
       if other.zero? then return self; end
       self.prev - other.prev
     end
+    #x = q * y + r
     def divmod(other)
       raise TypeError unless other.kind_of?(self.class)
       if other.zero?
@@ -86,12 +95,8 @@ module NewNum
       end
       [q, self + other - tmp]
     end
-    def /(other)
-      divmod(other)[0]
-    end
-    def %(other)
-      divmod(other)[1]
-    end
+    def /(other); divmod(other)[0]; end
+    def %(other); divmod(other)[1]; end
   end
   N = NaturalNum
 end
